e8b447b065610b31d0c10e366735d9558c087b21
drumev33 <martin.drumev98@gmail.com>
2021-06-25 16:55:22 +0300

    Added 02pythoneasyprojects

---
+++ 02pythoneasyprojects/02madlibsgame.py
@@ -0,0 +1,9 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+
+print("Roses are " + color)
+print(plural_noun + " is blue")
+print("I love " + celebrity)
+
