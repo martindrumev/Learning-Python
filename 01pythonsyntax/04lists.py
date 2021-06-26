65228efbd6f8822d80d4b46c3c75609db8b3f416
drumev33 <martin.drumev98@gmail.com>
2021-06-26 18:13:27 +0300

    Updated lists

--- 01pythonsyntax/04lists.py
+++ 01pythonsyntax/04lists.py
@@ -7,3 +7,24 @@ print(mylist)
 
 mylist[0] += 1
 print(mylist)
+
+print()
+lucky_numbers = [4, 8, 44, 34, 44, 84]
+friends = ["Mark", "John", "Richard", "John"]
+#friends.extend(lucky_numbers)
+friends.append("Ilias")
+friends.insert(1, "Kelly")
+friends.remove("Mark")
+friends.pop()
+print(friends.count("John"))
+print(friends.index("Richard"))
+print()
+friends.sort()
+print(friends)
+print()
+
+friends2 = friends.copy()
+
+friends.clear()
+print(friends)
+
