5b5608b2cd574fc555fa65771551454056ed7b1e
drumev33 <martin.drumev98@gmail.com>
2021-07-15 08:21:57 +0300

    Updated 09PyQt_WebBrowser

--- 03beginner_to_advanced_python/09PyQt_WebBrowser/02interacting_tabs.py
+++ 03beginner_to_advanced_python/09PyQt_WebBrowser/02interacting_tabs.py
@@ -28,11 +28,12 @@ class App(QFrame):
     def CreateApp(self):
         self.layout = QVBoxLayout()
         self.layout.setSpacing(0)
-        self.layout.setContentsMargins(10, 10, 10, 10)
+        self.layout.setContentsMargins(1, 0, 0, 1)
 
         # Create Tabs
         self.tabbar = QTabBar(movable=True, tabsClosable=True)
         self.tabbar.tabCloseRequested.connect(self.CloseTab)
+        self.tabbar.tabBarClicked.connect(self.SwitchTab)
 
         self.tabbar.setCurrentIndex(0)
 
@@ -44,14 +45,15 @@ class App(QFrame):
         self.Toolbar = QWidget()
         self.ToolbarLayout = QHBoxLayout()
         self.addressbar = AddressBar()
+        self.AddTabButton = QPushButton("+")
+
+        self.addressbar.returnPressed.connect(self.BrowseTo)
 
         self.Toolbar.setLayout(self.ToolbarLayout)
         self.ToolbarLayout.addWidget(self.addressbar)
 
         # New tab button
-        self.AddTabButton = QPushButton("+")
         self.AddTabButton.clicked.connect(self.AddTab)
-
         self.ToolbarLayout.addWidget(self.AddTabButton)
 
         # set main view
@@ -83,6 +85,8 @@ class App(QFrame):
         self.tabs[i].content = QWebEngineView()
         self.tabs[i].content.load(QUrl.fromUserInput("http://google.com"))
 
+        self.tabs[i].content.titleChanged.connect(lambda: self.SetTabText(i))
+
         # Add webview to tabs layout
         self.tabs[i].layout.addWidget(self.tabs[i].content)
 
@@ -95,8 +99,61 @@ class App(QFrame):
 
         # Set the tab at top of screen
         self.tabbar.addTab("New Tab")
-        self.tabbar.setTabData("tab" + str(i))
+        #self.tabbar.setTabData(i, "tab" + str(i))
+        self.tabbar.setTabData(i, {"object": "tab" + str(i), "initial": i})
+
         self.tabbar.setCurrentIndex(i)
+
+        self.tabCount += 1
+
+    def SwitchTab(self, i):
+        tab_data = self.tabbar.tabData(i)
+        print("tab:", tab_data)
+
+        tab_content = self.findChild(QWidget, tab_data)
+        self.container.layout.setCurrentWidget(tab_content)
+
+    def BrowseTo(self):
+        text = self.addressbar.text()
+        print(text)
+
+        i = self.tabbar.currentIndex()
+        tab = self.tabbar.tabData(i)
+        wv = self.findChild(QWidget, tab).content
+
+        if "http" not in text:
+            if "." not in text:
+                url = "https://www.google.com/search?q=" + text
+            else:
+                url = "http://" + text
+        else:
+            url = text
+
+        wv.load(QUrl.fromUserInput(url))
+
+    def SetTabText(self, i):
+        '''
+            self.tabs[i].objectName = tab1
+            self.tabbar.tabData(i)["object"] = tab1
+        '''
+        tab_name = self.tabs[i].objectName()
+        # tab1
+
+        count = 0
+        running = True
+
+        while running:
+            tab_data_name = self.tabbar.tabData(count)
+
+            if count >= 99:
+                running = False
+            if tab_name == tab_data_name["object"]:
+                newTitle = self.findChild(QWidget, tab_name).content.title()
+                self.tabbar.setTabText(count, newTitle)
+                running = False
+            else:
+                count += 1
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
