1aeb20512ba189c2575b00ac6dd1c2599e63402c
drumev33 <martin.drumev98@gmail.com>
2021-07-17 00:14:20 +0300

    Updated 10Data_Visualization

--- 03beginner_to_advanced_python/10Data_Visualization/01world_population_graph.py
+++ 03beginner_to_advanced_python/10Data_Visualization/01world_population_graph.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
 
-years = [1, 100, 1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950,
+years = [1950,
          1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000,
          2005, 2010, 2015]
 
-pops = [200, 400, 458, 580, 682, 791, 1000, 1262, 1650, 2525, 2758, 3018, 3322, 3682,
-       4061, 4440, 4853, 5310, 5735, 6127, 6520, 6930, 7349]
+pops = [2.5, 2.7, 3.0, 3.3, 3.6,
+       4.0, 4.4, 4.8, 5.3, 5.7, 6.1, 6.5, 6.9, 7.3]
 
-plt.plot(years, pops)
+plt.plot(years, pops, color=(255/255, 100/255, 100/255))
+
+plt.ylabel("Population in billions")
+plt.xlabel("Population growth by year")
+
+plt.title("Population Growth")
 plt.show()
