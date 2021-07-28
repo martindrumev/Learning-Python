11041eb3fb48e2ebc9154e58d0a8c883d34b163a
drumev33 <martin.drumev98@gmail.com>
2021-07-28 15:58:53 +0300

    Added 05w3resources_exercises

---
+++ 05w3resources_exercises/08exercise.py
@@ -0,0 +1,17 @@
+# Exercise
+'''
+Write a Python program to display the first and last colors from the following list. Go to the editor
+color_list = ["Red","Green","White" ,"Black"]
+'''
+
+
+# My code
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0])
+print(color_list[2])
+
+# Answer
+'''
+color_list = ["Red","Green","White" ,"Black"]
+print( "%s %s"%(color_list[0],color_list[-1]))
+'''
