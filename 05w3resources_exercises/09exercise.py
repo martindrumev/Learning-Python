a8af145394d901afe8c2ea112a74ca9b83a32cd7
drumev33 <martin.drumev98@gmail.com>
2021-07-29 11:11:51 +0300

    Updated 05w3resources_exercises

---
+++ 05w3resources_exercises/09exercise.py
@@ -0,0 +1,17 @@
+# Exercise
+'''
+Write a Python program to display the examination schedule. (extract the date from exam_st_date). Go to the editor
+exam_st_date = (11, 12, 2014)
+Sample Output : The examination will start from : 11 / 12 / 2014
+'''
+
+
+# My code
+exam_st_date = (11, 12, 2014)
+print(exam_st_date[0], "/", exam_st_date[1], "/", exam_st_date[2])
+
+# Answer
+'''
+exam_st_date = (11,12,2014)
+print( "The examination will start from : %i / %i / %i"%exam_st_date)
+'''
